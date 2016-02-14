2facef3e38fa3e9fc08e15ad8d1b2340369038e3
Lukáš Dočkal <Lokishek@seznam.cz>
2016-02-14 01:13:16 +0100

    Revert "Revert "2_service_status""
    This reverts commit c219e4ffd2e8d926975cb1d5cb3123cd75dc4b63.

--- configuration.php
+++ configuration.php
@@ -8,9 +8,14 @@
  */
 
 
- define ("path", "/");
+session_start();
+
+define ("path", "./");
 
 define ("db_server_name", "localhost");
 define ('db_user_name', "root");
 define ('db_password', "");
 define ('db_name', "gangit");
+
+define ('page_status', '0'); //1 for Service status , 0 for enabled
+define ('access_service_key','S8g9sd'); //for hidden input in service page
